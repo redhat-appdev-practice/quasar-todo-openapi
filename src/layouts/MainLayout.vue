92f1e13bbb2e242f06ee7477bc0c0353001382ce
Deven Phillips <deven.phillips@redhat.com>
2022-05-06 09:33:11 -0400

    Removed API code from main

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -5,9 +5,6 @@
         <q-toolbar-title>
           DEVOXX Todo OpenAPI
         </q-toolbar-title>
-
-        <span class="user_name">{{ name }}</span>
-        <q-avatar><img :src="avatar"></q-avatar>
       </q-toolbar>
     </q-header>
 
@@ -17,28 +14,6 @@
   </q-layout>
 </template>
 
-<script setup lang="ts">
-
-import { inject, ref } from 'vue';
-import { AllApi } from 'src/sdk';
-import { useQuasar } from 'quasar';
-
-const $q = useQuasar();
-
-const api = inject<AllApi>('api');
-
-const avatar = ref<string|undefined>('');
-const name = ref<string|undefined>('');
-
-api?.getCurrentUser()
-  .then((res) => {
-    avatar.value = res.data.avatar;
-    name.value = res.data.name;
-  })
-  .catch((err) => {
-    $q.notify({ message: err.message, type: 'negative' });
-  });
-</script>
 <style lang="sass">
 .user_name
   color: $secondary
