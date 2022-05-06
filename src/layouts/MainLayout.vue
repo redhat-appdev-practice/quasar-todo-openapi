dfe1ae0333c4c63c9767ac8fd3bbf0380e51f591
Deven Phillips <deven.phillips@redhat.com>
2022-05-06 09:19:42 -0400

    Styled to match design prototype

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,8 +3,11 @@
     <q-header elevated>
       <q-toolbar>
         <q-toolbar-title>
-          Quasar Todo OpenAPI
+          DEVOXX Todo OpenAPI
         </q-toolbar-title>
+
+        <span class="user_name">{{ name }}</span>
+        <q-avatar><img :src="avatar"></q-avatar>
       </q-toolbar>
     </q-header>
 
@@ -15,4 +18,31 @@
 </template>
 
 <script setup lang="ts">
+
+import { inject, ref } from 'vue';
+import { AllApi } from 'src/sdk';
+import { useQuasar } from 'quasar';
+
+const $q = useQuasar();
+
+const api = inject<AllApi>('api');
+
+const avatar = ref<string|undefined>('');
+const name = ref<string|undefined>('');
+
+api?.getCurrentUser()
+  .then((res) => {
+    avatar.value = res.data.avatar;
+    name.value = res.data.name;
+  })
+  .catch((err) => {
+    $q.notify({ message: err.message, type: 'negative' });
+  });
 </script>
+<style lang="sass">
+.user_name
+  color: $secondary
+  margin-right: 0.4rem
+.q-toolbar__title
+  color: $secondary
+</style>
